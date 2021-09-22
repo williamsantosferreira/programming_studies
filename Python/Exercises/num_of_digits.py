cb11cb2daae65520485a22c00d54d4feb1dfef55
William Santos <60198276+williamsantosferreira@users.noreply.github.com>
2021-09-22 09:07:01 -0300

    Create num_of_digits.py

---
+++ Python/Exercises/num_of_digits.py
@@ -0,0 +1,8 @@
+'''
+Create a function that will return an integer number corresponding to the amount of digits in the given integer num.
+'''
+def num_of_digits(number):
+	return len(str(number))
+
+num = 0	
+print(num_of_digits(num))
